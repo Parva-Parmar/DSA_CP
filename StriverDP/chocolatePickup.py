527b15e3df7a90cd42a2e620f996f614ed35e41f
Harsh Bhagat <93080554+BhagatHarsh@users.noreply.github.com>
2023-06-11 17:01:55 +0530

    Update chocolatePickup.py

--- StriverDP/chocolatePickup.py
+++ StriverDP/chocolatePickup.py
@@ -11,40 +11,36 @@ from typing import List
 
 
 def maximumChocolates(r: int, c: int, grid: List[List[int]]) -> int:
-    n = len(grid)
-    m = len(grid[0])
+    front = [[0]*m for _ in range(m)]
+    cur = [[0]*m for _ in range(m)]
 
-    dp = []
-    for i in range(n):
-        dp.append([])
-        for j in range(m):
-            dp[i].append([])
-            for k in range(m):
-                dp[i][j].append(-1)
-
-    dj = [-1, 0, 1]
-    
-    for i in range(m):
-        for j in range(m):
-            if(i == j):
-                dp[0][i][j] = grid[0][i]
+    for j1 in range(m):
+        for j2 in range(m):
+            if j1 == j2:
+                front[j1][j2] = grid[n-1][j1]
             else:
-                dp[0][i][j] = grid[0][i] + grid[0][j]
+                front[j1][j2] = grid[n-1][j1] + grid[n-1][j2]
 
-    for i in range(1,n):
+    for i in range(n-2, -1, -1):
         for j1 in range(m):
             for j2 in range(m):
-                if(dp[i][j1][j2] != -1):
-                    continue
-                for k in dj:
-                    maxi = -1
-                    for l in dj:
-                        if(j1+k < 0 or j1+k >= m or j2+l < 0 or j2+l >= m):
-                            continue
-                        if(j1+k == j2+l):
-                            maxi = max(maxi, grid[i][j1+k] + dp[i-1][j1+k][j2+l])
+                maxi = -maxsize
+
+                for di in range(-1, 2):
+                    for dj in range(-1, 2):
+                        ans = 0
+                        if j1 == j2:
+                            ans = grid[i][j1]
                         else:
-                            maxi = max(maxi, grid[i][j1+k] + grid[i][j2+l] + dp[i-1][j1+k][j2+l])
-                        dp[i][j1+k][j2+l] = maxi
-    
-    return dp[-1][-1][0]
+                            ans = grid[i][j1] + grid[i][j2]
+
+                        if ((j1 + di < 0 or j1 + di >= m) or (j2 + dj < 0 or j2 + dj >= m)):
+                            ans += int(-1e9)
+                        else:
+                            ans += front[j1 + di][j2 + dj]
+                        maxi = max(ans, maxi)
+            cur[j1][j2] = maxi
+        front = cur.copy()
+
+    return front[0][m-1]
+
