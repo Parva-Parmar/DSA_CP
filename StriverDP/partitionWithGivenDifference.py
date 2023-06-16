39e6894c7c8a1b1079aeacc8c1a31cb302c76c22
Harsh Bhagat <93080554+BhagatHarsh@users.noreply.github.com>
2023-06-16 18:02:29 +0530

    16-5-23

---
+++ StriverDP/partitionWithGivenDifference.py
@@ -0,0 +1,36 @@
+'''
+https://www.codingninjas.com/codestudio/problems/partitions-with-given-difference_3751628?source=youtube&campaign=striver_dp_videos&leftPanelTab=0
+'''
+
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+from typing import List
+
+def findWays(num: List[int], tar: int) -> int:
+    n = len(num)
+    dp = [[0]*(tar+1) for i in range(n)]
+    if(num[0] == 0): 
+        dp[0][0] = 2
+    else: 
+        dp[0][0] = 1
+    if(num[0] != 0 and num[0] <= tar): 
+        dp[0][num[0]] = 1
+
+
+    for ind in range(1,n):
+        for target in range(tar+1):
+            notTake = dp[ind-1][target]
+            take = 0
+            if(num[ind] <= target):
+                take = dp[ind-1][target-num[ind]]
+            dp[ind][target] = (take + notTake) % int(10**9 + 7)
+    
+    return dp[n-1][tar]
+
+def countPartitions(n: int, d: int, arr: List[int]) -> int:
+    totalSum = sum(arr)
+    if((totalSum - d) % 2 == 1 or totalSum - d < 0): return 0
+    return findWays(arr, (totalSum - d) // 2 ) % int(10**9 + 7)
